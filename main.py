6fe344ab9910e1ae967b7201f1109a3e9596a418
arjun006 <arjun.devakumar66@gmail.com>
2021-05-13 02:55:03 -0400

    wooooow

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import discord
 import os
 from dotenv import load_dotenv
+import requests
+import json
 load_dotenv()
 
 client = discord.Client()
 # SECRET_KEY = os.environ.get("ODQyMjYxODgxOTkyMzgwNDE3.YJyvgQ.GESdQVmhAXqex4x3xbcwC664HcU")
-
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -15,7 +21,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$bussin'):
+        await message.channel.send('Sheeeeesh')
+
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
 
 client.run(os.getenv('TOKEN'))
