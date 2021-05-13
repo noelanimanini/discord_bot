3a3e48dd4af6b1ff9e64de29f054db7de4267131
crystal <noelani.villanueva@gmail.com>
2021-05-12 23:00:21 -0700

    discord bot works!!!

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+# import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = discord.Client()
+# SECRET_KEY = os.environ.get("ODQyMjYxODgxOTkyMzgwNDE3.YJyvgQ.GESdQVmhAXqex4x3xbcwC664HcU")
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('ODQyMjYxODgxOTkyMzgwNDE3.YJyvgQ.GESdQVmhAXqex4x3xbcwC664HcU')
+# client.run(os.getenv('TOKEN'))
